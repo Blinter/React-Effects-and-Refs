7846abd3c96a9ce584ed20a961467517bbab1c62
Blinter <90986012+Blinter@users.noreply.github.com>
2024-10-13 23:13:16 -0400

    fix: vite config path fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://blinter.github.io/React-Effects-and-Refs/React-Effects-and-Refs/'
+  base: 'https://blinter.github.io/React-Effects-and-Refs/'
 })
